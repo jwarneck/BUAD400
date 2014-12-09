e66926b6a68df876c689a19c7f0f9ccb660f4d3f
jwarneck <jordanwarnecke@yahoo.com>
2014-12-08 21:46:29 -0500

    HTML

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -1,13 +1,17 @@
-# Sets URL on carbuzz.com to reflect the make the USER wants
-# to search for.
-def set_make_url(make):
+# Sets URL on carbuzz.com to reflect the make and model the 
+# USER wants to search for.
+def set_model_url(make):
 	URL = "http://www.carbuzz.com/" + make +"/"
 	return URL
 
+# sets the model the USER wants to search
+# input is make, returns model
 def set_model(make):
-	model = input("Please enter the model " + make + " you want to search:")
+	model = raw_input("Please enter the model " + make + " you want to search:")
 	return model
 
+# sets the make the USER wants to search
+# returns make of car USER inputs
 def set_make():
-	make = input("Please enter the brand car you want to search: \n")
+	make = raw_input("Please enter the brand car you want to search:")
 	return make
--- final_project/final_test.py
+++ final_project/final_test.py
@@ -7,8 +7,14 @@ n = 10
 make = set_make()
 model = set_model(make)
 
-URL = set_make_url(make)
+URL = set_model_url(make)
 
 print("URL = " + URL)
-print("Make: " + make +" Model: " + model)
+print("Make:  " + make)
+print("Model: " + model)
 
+print(" ")
+
+# print raw HTML
+print("HTML")
+print(soup.prettify().encode('ascii', 'ignore'))
